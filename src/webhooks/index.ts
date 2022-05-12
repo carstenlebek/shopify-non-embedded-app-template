c2e45e879e9889c3e783ac9f6cadc95142d46856
Carsten Lebek <59960385+carstenlebek@users.noreply.github.com>
2022-05-12 22:01:30 +0200

    Removed MongoDB as session store

--- src/webhooks/index.ts
+++ src/webhooks/index.ts
@@ -1,9 +1,7 @@
 import dbConnect from "@lib/db";
 import ShopModel from "@lib/models/ShopModel";
 import mongoose from "mongoose";
-import SessionModel from "@lib/models/SessionModel";
 
-const sessionModel = mongoose.models.session || SessionModel
 const shopModel = mongoose.models.shop || ShopModel
 
 const webhooks = {
@@ -11,7 +9,6 @@ const webhooks = {
         path: '/api/webhooks',
         webhookHandler: async (topic, shop, body) => {
             await dbConnect()
-            await sessionModel.deleteMany({shop})
             await shopModel.deleteMany({shop})
             console.log("App uninstalled")
         }

57a2d54fcf0a9e606f2f829cb8ee0317bfc6dbed
Andrej Mazhenkovski <andrej.mazenkovski@gmail.com>
2023-03-15 15:08:32 +0100

    feat: Delete redis session on webhook uninstall

--- src/webhooks/index.ts
+++ src/webhooks/index.ts
@@ -1,10 +1,43 @@
-const webhooks = {
-    "APP_UNINSTALLED": {
-        path: '/api/webhooks',
-        webhookHandler: async (topic, shop, body) => {
-            console.log("App uninstalled")
-        }
-    }
-}
+const headers = {
+  Authorization: `Bearer ${process.env.UPSTASH_REDIS_REST_TOKEN}`,
+};
 
-export default webhooks
+const webhooks = {
+  APP_UNINSTALLED: {
+    path: '/api/webhooks',
+    webhookHandler: async (topic, shop, body) => {
+      console.log('App uninstalled');
+
+      async function deleteKeys() {
+        const keysResponse = await fetch(
+          `${process.env.upstashRedisRestUrl}/keys/*`,
+          {
+            headers,
+          }
+        );
+        const keys = await keysResponse.json();
+
+        for (const key of keys.result) {
+          const valueResponse = await fetch(
+            `${process.env.upstashRedisRestUrl}/get/${key}`,
+            { method: 'GET', headers }
+          );
+          const value = await valueResponse.text();
+          console.log('Value: ', value);
+          if (value.includes(shop)) {
+            console.log(`Deleting key: ${key}`);
+
+            await fetch(`${process.env.upstashRedisRestUrl}/del/${key}`, {
+              method: 'GET',
+              headers,
+            });
+          }
+        }
+      }
+
+      deleteKeys();
+    },
+  },
+};
+
+export default webhooks;
